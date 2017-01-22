b7784aebc77ef29b288cbbbbdd9ad40ad0ef085b
jola <jola.j.wk@gmail.com>
2017-01-22 19:47:02 +0100

    @Around

--- src/main/java/pl/spring/fibonacci/app/Main.java
+++ src/main/java/pl/spring/fibonacci/app/Main.java
@@ -1,13 +1,18 @@
 package pl.spring.fibonacci.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import pl.spring.fibonacci.config.FibonaccApplicationConfiguration;
 
 public class Main {
-    public static void main( String[] args ){
+    public static void main( String[] args ){       	
+    	
     	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
 				FibonaccApplicationConfiguration.class);
 		context.getBean(FibonacciAppRunner.class).start();
     }
+
 }
--- src/main/java/pl/spring/fibonacci/aspect/CacheAspect.java
+++ src/main/java/pl/spring/fibonacci/aspect/CacheAspect.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pl.spring.fibonacci.service.FibonacciService;
@@ -16,14 +17,20 @@ import pl.spring.fibonacci.service.FibonacciService;
 @Service
 public class CacheAspect{
 
+	@Autowired
 	private FibonacciService fb;
 	private Map<Integer, BigInteger> cache;
 
 	@Around("execution(* pl.spring.fibonacci.service.impl.*.*(..))")
 	public Object catchTime(ProceedingJoinPoint pjp) {
+		cache = new HashMap<Integer, BigInteger>();
 		Object[] args = pjp.getArgs();
 		if(!cache.containsKey(args[0])){
-			cache.put((Integer) args[0], fb.fib((Integer) args[0]));
+			try {
+				cache.put((Integer) args[0], (BigInteger) pjp.proceed());
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
 		}
 		return cache.get(args[0]);
 	}
